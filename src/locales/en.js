7b78112164fe4fb82b047753a5560e5fe28ceacf
Peter Herlin <petorious@gmail.com>
2017-10-05 11:47:39 -0500

    Revert "small name change"
    This reverts commit 44268c34227b950af6897f55ff792b2d1c3abfab.

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,7 +1,7 @@
 import {defineMessages} from 'react-intl';
 
 const messages=defineMessages({
-  app_name: 'dmprov',
+  app_name: 'React Most Wanted',
   dashboard: 'Dashboard',
   about: 'About',
   page_not_found_demo: 'Page not found demo',
